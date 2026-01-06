ff5d5fc8b22d6c1963ab9c89d423e6fcf953ad4a
Juan Bosque <juan_bosque@hotmail.com>
2026-01-05 22:57:54 -0300

    feat(add):domain/interaces

---
+++ Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using Library_WinForms.Domain.Entities;
+using Library_WinForms.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_WinForms.Domain.Interfaces
+{
+    internal interface IBookRepository
+    {
+        List<BookDTO> GetBooks(AuthorDTORecomended filter);
+        BookDTO GetBookById(int code);
+        int UpdateBook(BookDTO book);
+        int InsertBook(BookDTO obook);
+        int DeleteBook(int codigo);
+        List<int> GetBookByYear();
+    }
+}
---
+++ Domain/Interfaces/ISexRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library_WinForms.Domain.DTOs;
+using Library_WinForms.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_WinForms.Domain.Interfaces
+{
+    internal interface ISexRepository
+    {
+        List<SexDTO> GetSexes();
+    }
+}
