c6942c63e6809f91e2fbb616ffa0ab207aa2920d
Juan Bosque <juan_bosque@hotmail.com>
2026-01-07 10:23:21 -0300

    feat(entities): implement domain models for Author, book, language, genre, nationality, publisher & sex

---
+++ Domain/Entities/Sex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Library_WinForms.Domain.Entities
+{
+    public class Sex
+    {
+        private int idSex;
+        private string sexType = string.Empty;
+
+        public int IdSex
+        {
+            get { return idSex; }
+            set { idSex = value; }
+        }
+
+        public string SexType
+        {
+            get { return sexType; }
+            set { sexType = value; }
+        }
+        public override string ToString()
+        {
+            return sexType;
+        }
+
+
+    }
+}
