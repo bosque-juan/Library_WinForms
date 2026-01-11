d6c9473718d597f86403059e0d2b2a26b0ba66be
Juan Bosque <juan_bosque@hotmail.com>
2026-01-10 21:32:54 -0300

    feat(add): domain interfaeces for IAuthor, IBook, IGenre, ILanguage, INationality, IPublisher, ISex

---
+++ Domain/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Library_WinForms.Aplication.DTOs;
+using Library_WinForms.Domain.Entities;
+using System.Collections.Generic;
+
+
+namespace Library_WinForms.Domain.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        List<Author> GetAuthors();
+        Author GetAuthorById(int id);
+        List<Author> GetAuthorsByFilter(AuthorDTORecomended filter);
+        int InsertAuthor(Author author);
+        int UpdateAuthor(Author author);
+        int DeleteAuthor(int id);
+    }
+}
---
+++ Domain/Interfaces/IPublisherRepository.cs
@@ -0,0 +1,18 @@
+﻿using Library_WinForms.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_WinForms.Domain.Interfaces
+{
+    internal interface IPublisherRepository
+    {
+        List<Publisher> GetPublishers();
+        Publisher GetPublisherById(int id);
+        int InsertPublisher(Publisher Publisher);
+        int UpdatePublisher(Publisher Publisher);
+        int DeletePublisher(int id);
+    }
+}
